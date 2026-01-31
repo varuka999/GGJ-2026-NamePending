c069c0020bc31d356bebd77deccd8028cc7d9865
varuka999 <vjkpokemon@yahoo.com>
2026-01-30 17:01:16 -0800

    UiManager testing
    - Notes: UI needs to be clamped to Clue
    - Probably make Clue a prefab., find someway to make it not a text prompt?

--- GameJam_2026/Assets/Scripts/UIManager.cs
+++ GameJam_2026/Assets/Scripts/UIManager.cs
@@ -9,7 +9,7 @@ public class UIManager : MonoBehaviour
     static public UIManager Instance { get { return instance; } }
 
     public event Action<string, Transform> DisplayClueTextEvent;
-    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject canvasPrefab;
     [SerializeField] private TMP_Text displayText = null;
 
     public void Awake()
@@ -38,9 +38,8 @@ public class UIManager : MonoBehaviour
 
     public void DisplayClueText(string clueString, Transform clue)
     {
-        TMP_Text textToDisplay = Instantiate(displayText, canvas.transform);
+        TMP_Text textToDisplay = Instantiate(displayText, canvasPrefab.transform);
         textToDisplay.transform.position = Camera.main.WorldToScreenPoint(clue.position) + new Vector3(50.0f, 50.0f, 0f);
         textToDisplay.text = clueString;
     }
-
 }
