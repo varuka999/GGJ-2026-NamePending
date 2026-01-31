8d76ae55c074fdd65acb259f9e67234c07404066
varuka999 <vjkpokemon@yahoo.com>
2026-01-30 16:38:53 -0800

    UIManager p1

--- GameJam_2026/Assets/Scripts/UIManager.cs
+++ GameJam_2026/Assets/Scripts/UIManager.cs
@@ -1,16 +1,46 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static UIManager instance = null;
+    static public UIManager Instance { get { return instance; } }
+
+    public event Action<string, Transform> DisplayClueTextEvent;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private TMP_Text displayText = null;
+
+    public void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DisplayClueTextEvent += DisplayClueText;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        DisplayClueTextEvent -= DisplayClueText;
     }
+
+    public void RequestDisplayClueText(string clueString, Transform clue)
+    {
+        DisplayClueTextEvent?.Invoke(clueString, clue);
+    }
+
+    public void DisplayClueText(string clueString, Transform clue)
+    {
+        TMP_Text textToDisplay = Instantiate(displayText, canvas.transform);
+        textToDisplay.transform.position = Camera.main.WorldToScreenPoint(clue.position) + new Vector3(50.0f, 50.0f, 0f);
+        textToDisplay.text = clueString;
+    }
+
 }
